f0f1c1818a65e166966699535fc30afa4ab36350
Wicked7k <142902110+Wicked7k@users.noreply.github.com>
2024-04-28 17:04:58 -0300

    Update carta.js

--- carta.js
+++ carta.js
@@ -1,1 +1,5 @@
 
+document.querySelector('.hamburger').addEventListener('click', function() {
+    document.querySelector('.navbar ul').classList.toggle('active');
+    document.querySelector('.hamburger').classList.toggle('active');
+}); 
