0d9efc9e306be9ca500e9054309729e5d6030c47
Wicked7k <142902110+Wicked7k@users.noreply.github.com>
2024-04-28 17:05:21 -0300

    Update reservar.js

--- reservar.js
+++ reservar.js
@@ -1,1 +1,4 @@
-
+document.querySelector('.hamburger').addEventListener('click', function() {
+    document.querySelector('.navbar ul').classList.toggle('active');
+    document.querySelector('.hamburger').classList.toggle('active');
+}); 
