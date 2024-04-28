e22c8514765ceec0e5b5b1c6885a802990c6d170
Wicked7k <142902110+Wicked7k@users.noreply.github.com>
2024-04-28 15:54:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.querySelector('.hamburger');
+    const navbar = document.querySelector('nav ul');
+
+    hamburger.addEventListener('click', function() {
+        navbar.classList.toggle('show-menu');
+    });
+
+    const menuItems = document.querySelectorAll('navbar');
+
+    menuItems.forEach(function(item) {
+        item.addEventListener('click', function() {
+            navbar.classList.remove('show-menu');
+        });
+    });
+});
 
