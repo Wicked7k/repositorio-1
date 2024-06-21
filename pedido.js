c16d4210c9b3ae1c9704cfb0bb72d38961004a2d
Wicked7k <142902110+Wicked7k@users.noreply.github.com>
2024-06-21 17:10:24 -0300

    Update pedido.js

--- pedido.js
+++ pedido.js
@@ -1,31 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
     const selectProductos = document.getElementById('productos');
+    const btnEliminarProducto = document.getElementById('eliminar-producto');
 
     let carrito = JSON.parse(sessionStorage.getItem('carrito')) || [];
 
     function actualizarProductosSeleccionados() {
         selectProductos.innerHTML = '';
 
-        carrito.forEach(producto => {
+        carrito.forEach((producto, index) => {
             const opcion = document.createElement('option');
             opcion.text = `${producto.nombre} - $${producto.precio.toFixed(2)}`;
-            opcion.value = `${producto.nombre}:${producto.precio}`;
+            opcion.value = index; 
             selectProductos.appendChild(opcion);
         });
     }
 
     actualizarProductosSeleccionados();
 
-    selectProductos.addEventListener('change', function(event) {
-        const selectedOption = event.target.value;
-        
-        if (selectedOption === '') {
-            selectProductos.selectedIndex = -1; 
+    btnEliminarProducto.addEventListener('click', function() {
+        const selectedIndex = selectProductos.selectedIndex;
+        if (selectedIndex !== -1) {
+            carrito.splice(selectedIndex, 1); 
+            sessionStorage.setItem('carrito', JSON.stringify(carrito)); 
+            actualizarProductosSeleccionados(); 
+        } else {
+            alert('Por favor, seleccione un producto para eliminar.');
         }
     });
 
     window.addEventListener('beforeunload', function(event) {
-        sessionStorage.removeItem('carrito');
+        sessionStorage.removeItem('carrito'); 
     });
 
     document.getElementById('formulario-pedido').addEventListener('submit', function(event) {
@@ -38,13 +42,13 @@ document.addEventListener('DOMContentLoaded', function() {
         const productosSeleccionados = Array.from(selectProductos.selectedOptions).map(option => option.value);
 
 
-        sessionStorage.removeItem('carrito');
-
-        window.location.href = "exito.html";
-    });
+        sessionStorage.removeItem('carrito'); 
+        window.location.href = "exito.html"; 
+    })
 
     document.querySelector('.hamburguer').addEventListener('click', function() {
         document.querySelector('.navbar ul').classList.toggle('active');
         document.querySelector('.hamburguer').classList.toggle('active');
     });
 });
+
